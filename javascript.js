cc0c71fdcc39cf744e33bd4c364a21bafa6fb69d
Gabriel Souza <141876347+Gbvnsz@users.noreply.github.com>
2024-11-13 09:02:39 -0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,13 @@
+// Animação de rolar a pági
+window.addEventListener('scroll', function() {
+    const elements = document.querySelectorAll('.fade-in');
+    const windowHeight = window.innerHeight;
+
+    elements.forEach(element => {
+        const elementTop = element.getBoundingClientRect().top;
+
+        if (elementTop < windowHeight - 100) {
+            element.classList.add('visible');
+        }
+    });
+});
